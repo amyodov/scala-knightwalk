fd3c77fac605e560e20bf7ebb3788513b5ddab3e
Alex Myodov <amyodov@gmail.com>
2020-09-15 19:03:55 +0300

    Now benchmarking is now in solution per-second; around 0.009 per solution.

--- knightwalk/src/main/scala/com/myodov/knightwalk/Launcher.scala
+++ knightwalk/src/main/scala/com/myodov/knightwalk/Launcher.scala
@@ -1,9 +1,29 @@
 package com.myodov.knightwalk
 
 object Launcher {
+  val ITERATIONS = 1000
+
   def main(args: Array[String]) = {
     val initPos = Travel(Pos("A8"))
+
+    // Benchmark
+
+    val startTime = System.nanoTime
+    val allResultValid = Range(0, ITERATIONS)
+      .map(_ => initPos.findKnightTravelSolution)
+      .forall(res => res.nonEmpty)
+    val endTime = System.nanoTime
+
+    val durationSeconds = (endTime - startTime) / 1_000_000_000.0
+    val tps = ITERATIONS / durationSeconds
+    val secPerIter = durationSeconds / ITERATIONS
+
+    println(s"Tried $ITERATIONS iterations, all knight travels found: $allResultValid.")
+    println(f"$tps%.2f solutions per second, each took $secPerIter%.6f sec.")
+
+    // Find reference solution
     val solution = initPos.findKnightTravelSolution
+
     if (solution.nonEmpty) {
       println(s"Solution is: ${solution.get}")
     } else {
--- knightwalk/src/main/scala/com/myodov/knightwalk/Travel.scala
+++ knightwalk/src/main/scala/com/myodov/knightwalk/Travel.scala
@@ -1,7 +1,5 @@
 package com.myodov.knightwalk
 
-import java.util.concurrent.atomic.AtomicInteger
-
 import scala.collection.immutable.ListSet
 
 /** Some particular travel of a knight over the chess board. */
@@ -38,18 +36,6 @@ case class Travel(visited: ListSet[Pos]) {
    * Return as soon as solution has been found.
    * */
   lazy val findKnightTravelSolution: Option[Travel] = {
-    val checkpointSize = 5000000
-
-    val counter = Travel.counter.incrementAndGet()
-
-    if (counter % checkpointSize == 0) {
-      val thisTS = System.nanoTime
-      val durationSeconds = (thisTS - Travel.startTS) / 1_000_000_000.0
-      val tps = counter / durationSeconds
-
-      println(f"$counter; $tps%.0f TPS")
-    }
-
     if (visitedAllBoard) {
       // Solution found in this branch of travels!
       Some(this)
@@ -73,7 +59,4 @@ object Travel {
 
   /** Constructor like `Travel(Pos("D4"), Pos("E6"), Pos("F4")).` */
   final def apply(visited: Pos*) = new Travel(visited: _*)
-
-  val startTS = System.nanoTime
-  val counter = new AtomicInteger(0)
 }
